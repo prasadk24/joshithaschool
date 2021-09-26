5c4420e90df5363e9e5b781226c3d8e4e5fe35a8
prasadk24 <prasad.k24@gmail.com>
2021-09-26 09:04:27 +0530

    header section changed

--- js/common.js
+++ js/common.js
@@ -1,8 +1,16 @@
  function myFunction() {
-    var x = document.getElementById("navbarResponsive");
-    if (x.className === "navbar-nav") {
-      x.className += " responsive";
-    } else {
-      x.className = "navbar-nav";
-    }
-  }
+     var x = document.getElementById("navbarResponsive");
+     if (x.className === "navbar-nav") {
+         x.className += " responsive";
+     } else {
+         x.className = "navbar-nav";
+     }
+ }
+
+ fetch("reuseheader.html")
+     .then(response => {
+         return response.text()
+     })
+     .then(data => {
+         document.querySelector("header").innerHTML = data;
+     });
